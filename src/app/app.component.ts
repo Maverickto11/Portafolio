025e7cdfab584a1004acd57428e90404e9517b2b
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-02-02 21:28:19 -0500

    Carpeta de Skills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SkillsComponent } from './Skills/skills.component';
 import { MatDialog } from '@angular/material/dialog';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ProyectosComponent]
+    imports: [RouterOutlet, ProyectosComponent, SkillsComponent]
 })
 export class AppComponent {
   constructor(public dialog: MatDialog) {}
   backgroundImagePath = 'assets/img/Escritorio.webp';
   folderImagePath = 'assets/img/Carpeta.png';
+  skills= './assets/img/Skills.png';
+
   
   openModal(): void {
     const dialogRef = this.dialog.open(ProyectosComponent, {
@@ -23,4 +26,13 @@ export class AppComponent {
       console.log('The dialog was closed');
     });
   }
+  openModalSkills(): void {
+    const dialogRef = this.dialog.open(SkillsComponent, {
+      /*width: '23cm'*/
+      });
+      
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+      });
+    }
 }
